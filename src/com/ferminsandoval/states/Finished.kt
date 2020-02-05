2ad11f67ac1a807bab6ee7aea6020374a6044398
Fermin Sandoval <ferminsandoval950@hotmail.com>
2020-02-04 22:26:15 -0700

    Implemented Finished

--- src/com/ferminsandoval/states/Finished.kt
+++ src/com/ferminsandoval/states/Finished.kt
@@ -3,7 +3,5 @@ package com.ferminsandoval.states
 import com.ferminsandoval.models.Instruction
 
 class Finished : State{
-    override fun nextState(instruction: Instruction): State {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun nextState(instruction: Instruction): State = this
 }
